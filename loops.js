ac2a278b79e2ebd1e95bedbfa53d2457789cbb0d
i-dont-have-any-good-names <67664504+i-dont-have-any-good-names@users.noreply.github.com>
2022-11-22 07:54:13 +0000

    n/a

---
+++ loops.js
@@ -0,0 +1,24 @@
+var math = 1;
+var k = true;
+
+if(k = true) {
+    console.log("var k =", k);
+};
+
+console.log("staring loop");
+while(k == true) {
+    console.log(math++);
+    if(math > 100) {
+        console.log("loop ended")
+        var k = false;
+        break;
+    };
+};
+
+if(k == false) {
+    console.log("var k =", k);
+} else {
+    console.log("var k =", k);
+};
+
+console.warn("finished");
